eb444d8ad2802e1c14354fdec27393b66fb7f976
Manimaran <Manimaran@ManimaranEs-MacBook-Pro.local>
2020-03-29 13:50:09 +0800

    Udemy Exercise

---
+++ UdemyExercise.py
@@ -0,0 +1,6 @@
+print ("My Hovercraft is Full of Eels")
+print ("Hello" + "World")
+print ("hello" + ' ' + "World")
+greeting = "hello"
+name = input ("Please input your name: ")
+print (greeting + ' ' + name)
